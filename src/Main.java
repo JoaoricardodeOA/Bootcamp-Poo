15e24541a5f1e4b35ad6c76598e40ed1c2bb68e1
joaoricardodeOA <joaoricardoagostinho285@gmail.com>
2022-01-21 17:09:53 -0300

    bootcamp, dev e conteudo(abstrato).

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import domain.Bootcamp;
 import domain.Curso;
+import domain.Dev;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +9,21 @@ public class Main {
         curso1.setCargaHoraria(2);
         curso1.setDescricao("Nesse curso você será capaz de criar aplicaçãoes com Poo");
         System.out.println(curso1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java developer");
+        bootcamp.setDescricao("Aprenda a ser java developer");
+        bootcamp.getConteudos().add(curso1);
+
+        Dev camila= new Dev();
+        camila.setNome("cami");
+        System.out.println("Conteudos inscritos cami "+camila.getConteudosInscritos());
+        camila.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteudos inscritos cami "+camila.getConteudosInscritos());
+        camila.progredir();
+        System.out.println("Conteudos concluidos cami "+camila.getConteudosConcluidos());
+        System.out.println(camila.calcularTotalXP());
+
     }
 }
